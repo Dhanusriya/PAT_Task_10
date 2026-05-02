0662707019859a9856c39a081d75112f49678ec5
Dhanusriya <dhanusriyasugananth@gmail.com>
2026-05-02 20:44:18 +0530

    Added testcases for the given cases

--- test_saucedemo.py
+++ test_saucedemo.py
@@ -1,38 +1,63 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import time
 
-# function that tests the valid login scenario - positive testcase
-def test_valid_login():
-    driver = webdriver.Chrome()
-    driver.get("https://www.saucedemo.com/")
-    driver.maximize_window()
-    time.sleep(2)
 
+#  FIXTURE
+@pytest.fixture
+def driver():
+    options = Options()
+    options.add_argument("--start-maximized")
+
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://www.saucedemo.com/")
+
+    yield driver
+    driver.quit()
+
+
+# ---------- TEST CASE 1: TITLE (POSITIVE) ----------
+def test_title_positive(driver):
+    assert "Swag Labs" in driver.title
+
+
+# ---------- TEST CASE 2: TITLE (NEGATIVE) ----------
+def test_title_negative(driver):
+    assert "Flipkart" not in driver.title
+
+
+# ---------- TEST CASE 3: HOMEPAGE URL (POSITIVE) ----------
+def test_homepage_url_positive(driver):
+    assert driver.current_url == "https://www.saucedemo.com/"
+
+
+# ---------- TEST CASE 4: HOMEPAGE URL (NEGATIVE) ----------
+def test_homepage_url_negative(driver):
+    assert driver.current_url != "https://www.google.com/"
+
+
+# ---------- TEST CASE 5: DASHBOARD URL (POSITIVE) ----------
+def test_dashboard_url_positive(driver):
+    # Login
     driver.find_element(By.ID, "user-name").send_keys("standard_user")
     driver.find_element(By.ID, "password").send_keys("secret_sauce")
     driver.find_element(By.ID, "login-button").click()
-    time.sleep(3)
-
-    assert "inventory" in driver.current_url
-    assert "Swag Labs" in driver.title
-
-    driver.quit()
-
-# function that tests the invalid login scenario - negative testcase
-def test_invalid_login():
-    driver = webdriver.Chrome()
-    driver.get("https://www.saucedemo.com/")
-    driver.maximize_window()
 
     time.sleep(2)
 
+    assert "inventory" in driver.current_url
+
+
+# ---------- TEST CASE 6: DASHBOARD URL (NEGATIVE) ----------
+def test_dashboard_url_negative(driver):
+    # Login with correct creds
     driver.find_element(By.ID, "user-name").send_keys("standard_user")
-    driver.find_element(By.ID, "password").send_keys("wrong_pass")
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
     driver.find_element(By.ID, "login-button").click()
-    time.sleep(3)
 
-    error = driver.find_element(By.XPATH, "//h3").text
-    assert "Epic sadface" in error
+    time.sleep(2)
 
-    driver.quit()
+    # Wrong expectation
+    assert "dashboard" not in driver.current_url
